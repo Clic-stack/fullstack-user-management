536257d2105aa491609705a65cfbd605af5f1207
Clic-stack <175146257+Clic-stack@users.noreply.github.com>
2026-04-20 19:10:28 -0600

    docs: modify comments in code

--- frontend/src/components/UserCard.jsx
+++ frontend/src/components/UserCard.jsx
@@ -8,7 +8,7 @@ function UserCard({user, onEdit, onDelete}) {
   // Función para manejar el error de carga de imagen
   const handleImageError = (e) => {
     // Si la imagen principal falla por el DNS, 
-    // cargamos una alternativa que sepamos que SÍ funciona.
+    // se carga una alternativa que sepamos que SÍ funciona.
     // Usaremos UI Avatars que es muy fiable.
     
     e.target.src = `https://ui-avatars.com/api/?name=${user.first_name}+${user.last_name}&background=F43F5E&color=fff&bold=true&size=128`;
@@ -18,7 +18,7 @@ function UserCard({user, onEdit, onDelete}) {
   const imageStyles = {
     borderRadius: '50%', 
     objectFit: 'cover',
-    border: '3px solid #00F5FF', // Un pequeño borde para que resalte en tu fondo oscuro
+    border: '3px solid #00F5FF',
     marginBottom: '15px'
   };
 
